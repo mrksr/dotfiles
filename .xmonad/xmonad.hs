d5fe414e45748f8a774204e73139ef33c454e8ab
Markus Kaiser <markus.kaiser@in.tum.de>
2013-03-27 13:07:20 +0100

    reindent

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,67 +16,66 @@ import XMonad.Layout.Tabbed
 import XMonad.Layout.Named
 import XMonad.Layout.ResizableTile
 
-myMod = mod4Mask    -- Super
-term = "urxvt"
+myMod = mod4Mask -- Super
+term  = "urxvt"
+dmenu = "dmenu_run -i -p 'run' -nb '#000' -nf '#4d6d99' -sb '#333' -sf '#cc5214'"
 
 myWorkspaces =
     ["Stuff", "Browser", "Term", "IM" ,"Mail", "Hannah", "J", "SoilentGreen", "MMXIII"]
 
 myManageHook = composeAll [
-      className =? "Pidgin" --> doShift "IM"
-    , className =? "Thunderbird" --> doShift "Mail"
-    , className =? "Firefox" --> doShift "Browser"
-    , className =? "Opera" --> doShift "Browser"
+      className =? "Pidgin"           --> doShift "IM"
+    , className =? "Thunderbird"      --> doShift "Mail"
+    , className =? "Firefox"          --> doShift "Browser"
+    , className =? "Opera"            --> doShift "Browser"
     , className =? "Chormium-browser" --> doShift "Browser"
-    , className =? "Gvim" --> doShift "Stuff"
-    , className =? "Smplayer" --> doFloat
-    , className =? "Xfce4-notifyd" --> doIgnore
-    , className =? "Gimp-2.8" --> doShift $ myWorkspaces !! 5
-    , className =? "Gimp-2.8" --> doFloat
+    , className =? "Gvim"             --> doShift "Stuff"
+    , className =? "Smplayer"         --> doFloat
+    , className =? "Xfce4-notifyd"    --> doIgnore
+    , className =? "Gimp-2.8"         --> doShift $ myWorkspaces !! 5
+    , className =? "Gimp-2.8"         --> doFloat
     ]
 
 myKeys = [
-        -- M1 ~ Alt
-        -- Special Keys
-          ("<Print>", spawn "scrot")
-        , ("<XF86AudioLowerVolume>", void $ lowerVolume 3)
-        , ("<XF86AudioRaiseVolume>", void $ raiseVolume 3)
-        , ("<XF86AudioPlay>", spawn "mpc toggle")
-        , ("<XF86AudioStop>", spawn "mpc stop")
-        , ("<XF86AudioNext>", spawn "mpc next")
-        , ("<XF86AudioPrev>", spawn "mpc prev")
-        , ("<XF86Sleep>", spawn "sudo pm-suspend")
-        , ("<XF86ScreenSaver>", spawn "gnome-screensaver-command -l")
+    -- M1 ~ Alt
+    -- Special Keys
+      ("<Print>", spawn "scrot")
+    , ("<XF86AudioLowerVolume>", void $ lowerVolume 3)
+    , ("<XF86AudioRaiseVolume>", void $ raiseVolume 3)
+    , ("<XF86AudioPlay>", spawn "mpc toggle")
+    , ("<XF86AudioStop>", spawn "mpc stop")
+    , ("<XF86AudioNext>", spawn "mpc next")
+    , ("<XF86AudioPrev>", spawn "mpc prev")
+    , ("<XF86Sleep>", spawn "sudo pm-suspend")
+    , ("<XF86ScreenSaver>", spawn "gnome-screensaver-command -l")
 
-        -- Spawns
-        , ("M-o", spawn "gnome-screensaver-command -l")
-        , ("M-v", spawn "pavucontrol")
-        , ("M-f", spawn "gvim ~/.xmonad/xmonad.hs ~/.xmobarrc ~/.vimrc")
-        , ("M-p", spawn dmenu)
-        , ("M1-<F12>", spawn dmenu)
-        , ("<F12>", spawn "kupfer")
-        , ("M-d", spawn term)
+    -- Spawns
+    , ("M-o", spawn "gnome-screensaver-command -l")
+    , ("M-v", spawn "pavucontrol")
+    , ("M-f", spawn "gvim ~/.xmonad/xmonad.hs ~/.xmobarrc ~/.vimrc")
+    , ("M-p", spawn dmenu)
+    , ("M1-<F12>", spawn dmenu)
+    , ("<F12>", spawn "kupfer")
+    , ("M-d", spawn term)
 
-        -- Windows
-        , ("M1-<F4>", kill)
-        , ("M-c", kill)
-        , ("M1-<Return>", windows W.shiftMaster)
-        , ("M1-<Tab>", windows W.focusDown)
-        , ("M1-S-<Tab>", windows W.focusUp)
-        , ("M-g", goToSelected defaultGSConfig)
-        , ("M-a", windows copyToAll)
-        , ("M-S-a", killAllOtherCopies)
+    -- Windows
+    , ("M1-<F4>", kill)
+    , ("M-c", kill)
+    , ("M1-<Return>", windows W.shiftMaster)
+    , ("M1-<Tab>", windows W.focusDown)
+    , ("M1-S-<Tab>", windows W.focusUp)
+    , ("M-g", goToSelected defaultGSConfig)
+    , ("M-a", windows copyToAll)
+    , ("M-S-a", killAllOtherCopies)
 
-        -- Workspaces
-        , ("M-^", toggleWS)
-        , ("M-<Tab>", moveTo Next NonEmptyWS)
-        , ("M-S-<Tab>", moveTo Prev NonEmptyWS)
-        , ("M-s", sendMessage ToggleStruts)
-        , ("M-S-h", sendMessage MirrorShrink)
-        , ("M-S-l", sendMessage MirrorExpand)
-        ]
-        where
-        dmenu = "dmenu_run -i -p 'run' -nb '#000' -nf '#4d6d99' -sb '#333' -sf '#cc5214'"
+    -- Workspaces
+    , ("M-^", toggleWS)
+    , ("M-<Tab>", moveTo Next NonEmptyWS)
+    , ("M-S-<Tab>", moveTo Prev NonEmptyWS)
+    , ("M-s", sendMessage ToggleStruts)
+    , ("M-S-h", sendMessage MirrorShrink)
+    , ("M-S-l", sendMessage MirrorExpand)
+    ]
 
 myLayout = onWorkspace "Term" (tabs ||| vs) $
            Full ||| tabs ||| vs ||| hs
@@ -90,15 +89,15 @@ myLayout = onWorkspace "Term" (tabs ||| vs) $
     ratio2   = 23/34
     tabTheme = defaultTheme {
                   inactiveBorderColor = "#333"
-                , activeBorderColor = "#333"
-                , activeColor = "#111"
-                , inactiveColor = "#000"
-                , inactiveTextColor = "#4d6d99"
-                , activeTextColor = "#a64211"
-                , fontName = "xft:Ubuntu Mono-9:bold"
-                , decoHeight = 16
-                , urgentColor = "#000"
-                , urgentTextColor = "#638046"
+                , activeBorderColor   = "#333"
+                , activeColor         = "#111"
+                , inactiveColor       = "#000"
+                , inactiveTextColor   = "#4d6d99"
+                , activeTextColor     = "#a64211"
+                , fontName            = "xft:Ubuntu Mono-9:bold"
+                , decoHeight          = 16
+                , urgentColor         = "#000"
+                , urgentTextColor     = "#638046"
                 }
 
 myStartup = do
@@ -123,23 +122,23 @@ myStartup = do
     spawnK toKill toSpawn = spawn $ "pkill " ++ toKill ++ ";" ++ toSpawn
 
 main = do
-  xmproc <- spawnPipe "xmobar"
-  xmonad $ defaultConfig {
-         borderWidth = 2
-       , terminal    = term
-       , workspaces  = myWorkspaces
-       , modMask     = myMod
-       , normalBorderColor = "#333"
-       , focusedBorderColor = "#500000"
-       , manageHook = manageHook defaultConfig <+> manageSpawn <+> manageDocks <+> myManageHook
-       , layoutHook = smartBorders . avoidStruts $ myLayout
-       , startupHook = myStartup
-       , logHook = dynamicLogWithPP xmobarPP {
-                          ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#aaaaaa" "" . shorten 150
-                        , ppCurrent = xmobarColor "#eeeeec" ""
-                        , ppSep   = " λ "
-                        , ppHidden = xmobarColor "#aaaaaa" ""
-                        , ppLayout  = xmobarColor "#aaaaaa" ""
-                        }
-       } `additionalKeysP` myKeys
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig {
+            borderWidth        = 2
+          , terminal           = term
+          , workspaces         = myWorkspaces
+          , modMask            = myMod
+          , normalBorderColor  = "#333"
+          , focusedBorderColor = "#500000"
+          , manageHook         = manageHook defaultConfig <+> manageSpawn <+> manageDocks <+> myManageHook
+          , layoutHook         = smartBorders . avoidStruts $ myLayout
+          , startupHook        = myStartup
+          , logHook            = dynamicLogWithPP xmobarPP {
+                    ppOutput   = hPutStrLn xmproc
+                  , ppTitle    = xmobarColor "#aaaaaa" "" . shorten 150
+                  , ppCurrent  = xmobarColor "#eeeeec" ""
+                  , ppSep      = " λ "
+                  , ppHidden   = xmobarColor "#aaaaaa" ""
+                  , ppLayout   = xmobarColor "#aaaaaa" ""
+          }
+    } `additionalKeysP` myKeys
