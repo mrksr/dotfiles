52f2afba1452cf5d8c0a7ceb8e8412a6e3e6fc4e
Markus Kaiser <markus.kaiser@in.tum.de>
2013-09-02 21:11:54 +0200

    Add hgrc; add simplistic init script

---
+++ .bootstrap.sh
@@ -0,0 +1,6 @@
+# Clone repos
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+mkdir -p ~/.hg_ext
+hg clone http://bitbucket.org/sjl/hg-prompt/ ~/.hg_ext/hg-prompt
