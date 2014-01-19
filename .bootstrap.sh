f2d033d3fbc0b9964e02e718ecf4c351d2ff370c
Markus Kaiser <markus.kaiser@in.tum.de>
2014-01-19 14:51:07 +0100

    do not create unused folder

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,7 +1,7 @@
 # Clone repos
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-mkdir -p ~/.hg_ext
+#mkdir -p ~/.hg_ext
 #hg clone http://bitbucket.org/sjl/hg-prompt/ ~/.hg_ext/hg-prompt
 
 # Symlinks
