f1d074e4f9c7db230b249c6eff7e2bba834b6516
Markus Kaiser <markus.kaiser@in.tum.de>
2014-07-09 20:40:51 +0200

    more mutt config

---
+++ .offlineimaprc.pass.py
@@ -0,0 +1,10 @@
+#!/usb/bin/python
+
+import locale
+from subprocess import Popen, PIPE
+
+encoding = locale.getdefaultlocale()[1]
+
+def get_password(user, server):
+    (out, err) = Popen(["secret-tool", "lookup", "user", user, "server", server], stdout=PIPE).communicate()
+    return out.decode(encoding).strip()
