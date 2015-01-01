8d1cfdac876e7263e5d1c2939c6c5f647eb4e1f5
Markus Kaiser <markus.kaiser@in.tum.de>
2015-01-01 19:30:58 +0100

    add imap as service to secret-tool

--- .offlineimaprc.pass.py
+++ .offlineimaprc.pass.py
@@ -7,5 +7,5 @@ from subprocess import Popen, PIPE
 encoding = locale.getdefaultlocale()[1]
 
 def get_password(user, server):
-    (out, err) = Popen(["secret-tool", "lookup", "user", user, "server", server], stdout=PIPE).communicate()
+    (out, err) = Popen(["secret-tool", "lookup", "service", "imap", "user", user, "server", server], stdout=PIPE).communicate()
     return out.decode(encoding).strip()
