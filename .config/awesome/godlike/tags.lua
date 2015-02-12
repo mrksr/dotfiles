ef689adfc148d92376860c6e09a26c4ea981d052
Markus Kaiser <markus.kaiser@in.tum.de>
2015-02-12 21:58:32 +0100

    remove floating layout; whitespace

--- .config/awesome/godlike/tags.lua
+++ .config/awesome/godlike/tags.lua
@@ -6,16 +6,16 @@ godlike.layouts = godlike.layouts or
 {
     awful.layout.suit.max,
     awful.layout.suit.tile,
-    --awful.layout.suit.tile.left,
+    -- awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
-    --awful.layout.suit.tile.top,
+    -- awful.layout.suit.tile.top,
     awful.layout.suit.fair,
-    --awful.layout.suit.fair.horizontal,
-    --awful.layout.suit.spiral,
+    -- awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
-    --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier,
-    awful.layout.suit.floating,
+    -- awful.layout.suit.max.fullscreen,
+    -- awful.layout.suit.magnifier,
+    -- awful.layout.suit.floating,
 }
 
 for s = 1, screen.count() do
