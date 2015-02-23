09baf9382852f1b3169279f19786dddf03941f95
Markus Kaiser <markus.kaiser@in.tum.de>
2015-02-23 22:09:35 +0100

    playerctl!

--- .config/awesome/godlike/audio.lua
+++ .config/awesome/godlike/audio.lua
@@ -2,10 +2,10 @@
 
 godlike.audio = godlike.audio or {}
 
-godlike.audio.play  = godlike.audio.play  or function() end
-godlike.audio.stop  = godlike.audio.stop  or function() end
-godlike.audio.next  = godlike.audio.next  or function() godlike.sexec("cmus-remote --next") end
-godlike.audio.prev  = godlike.audio.prev  or function() end
+godlike.audio.play  = godlike.audio.play  or function() godlike.sexec("playerctl play-pause") end
+godlike.audio.stop  = godlike.audio.stop  or function() godlike.sexec("playerctl stop") end
+godlike.audio.next  = godlike.audio.next  or function() godlike.sexec("playerctl next") end
+godlike.audio.prev  = godlike.audio.prev  or function() godlike.sexec("playerctl previous") end
 godlike.audio.mute  = godlike.audio.mute  or function() end
 godlike.audio.raise = godlike.audio.raise or function() godlike.sexec("amixer sset Master 8%+") end
 godlike.audio.lower = godlike.audio.lower or function() godlike.sexec("amixer sset Master 8%-") end
