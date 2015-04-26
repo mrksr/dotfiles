9dfabe9c18daeb3bda0e4f1a18027c799eb432d1
Markus Kaiser <markus.kaiser@in.tum.de>
2015-04-26 21:08:33 +0200

    fix umlauts in html mails

--- mail/.sup/hooks/mime-decode.rb
+++ mail/.sup/hooks/mime-decode.rb
@@ -3,6 +3,6 @@ unless sibling_types.member? "text/plain"
   case content_type
   when "text/html"
       # `/usr/bin/elinks -dump -no-numbering -no-references -no-home -default-mime-type #{content_type} #{Shellwords.escape filename}`
-      `w3m -I #{charset} -dump -T #{content_type} #{Shellwords.escape filename}`
+      `w3m -o alt_entity=0 -I #{charset} -O utf-8 -dump -T #{content_type} #{Shellwords.escape filename}`
   end
 end
