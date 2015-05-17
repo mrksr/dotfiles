ccefcc71b8e0d41172b3f63dbd6ee31215e1468f
Markus Kaiser <markus.kaiser@in.tum.de>
2015-05-17 17:56:34 +0200

    no need for the symlink anymore

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -4,6 +4,3 @@ git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 # .vim_local for undodir
 mkdir -p ~/.vim_local/undodir
-
-# Symlinks
-ln -s ~/.zsh-theme ~/.oh-my-zsh/themes/_mine.zsh-theme
