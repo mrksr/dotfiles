bbc3be83cfcc835fa0a86238b5097d3754e3ced0
Markus Kaiser <markus.kaiser@in.tum.de>
2015-05-17 17:57:02 +0200

    let stow create the .vim_local dir

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -1,6 +1,3 @@
 # Clone repos
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
-
-# .vim_local for undodir
-mkdir -p ~/.vim_local/undodir
