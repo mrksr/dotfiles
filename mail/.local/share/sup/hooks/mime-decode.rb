98302faad134737317cb2a46b7fbeee982a30ad6
Markus Kaiser <markus.kaiser@in.tum.de>
2016-02-28 20:27:27 +0100

    sup config files

---
+++ mail/.local/share/sup/hooks/mime-decode.rb
@@ -0,0 +1,8 @@
+require 'shellwords'
+unless sibling_types.member? "text/plain"
+  case content_type
+  when "text/html"
+      # `/usr/bin/elinks -dump -no-numbering -no-references -no-home -default-mime-type #{content_type} #{Shellwords.escape filename}`
+      `w3m -o alt_entity=0 -I #{charset} -O utf-8 -dump -cols 100 -T #{content_type} #{Shellwords.escape filename}`
+  end
+end
