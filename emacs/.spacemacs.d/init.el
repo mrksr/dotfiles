8548d2719e3c52923631bdf2118eb9d092c0bea4
Markus Kaiser <markus.kaiser@in.tum.de>
2017-03-29 19:21:11 +0200

    add python readline fix

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -250,6 +250,19 @@
     (add-hook 'find-file-hook 'undo-tree-load-history-hook)
     (add-hook 'find-file-hook 'global-undo-tree-mode-check-buffers)
     )
+
+  (progn
+    ;; python completion fix
+    (with-eval-after-load 'python
+      (defun python-shell-completion-native-try ()
+        "Return non-nil if can trigger native completion."
+        (let ((python-shell-completion-native-enable t)
+              (python-shell-completion-native-output-timeout
+               python-shell-completion-native-try-output-timeout))
+          (python-shell-completion-native-get-completions
+           (get-buffer-process (current-buffer))
+           nil "_"))))
+    )
   )
 
 
