781d42b3a0938ade47065903fbcf88059b9b399f
Markus Kaiser <markus.kaiser@in.tum.de>
2017-08-16 23:54:58 +0200

    When disabling maximized, also disable maximized_horizontal and _vertical

--- awesome/.config/awesome/godlike/clientkeys.lua
+++ awesome/.config/awesome/godlike/clientkeys.lua
@@ -6,15 +6,20 @@ local modkey = godlike.modkey
 godlike.clientkeys = awful.util.table.join(
     awful.key({ modkey,           }, "d",      function (c) c:kill()                         end),
     awful.key({ modkey,           }, "e",      function (c) c.fullscreen = not c.fullscreen  end),
-    awful.key({ modkey, "Shift"   }, "e",      function (c) c.maximized = not c.maximized  end),
+    awful.key({ modkey, "Shift"   }, "e",      function (c)
+                                                    c.maximized = not c.maximized
+                                                    if not c.maximized then
+                                                      c.maximized_horizontal = false
+                                                      c.maximized_vertical   = false
+                                                    end
+                                                end),
     awful.key({ modkey,           }, "r",      function (c) c.ontop = not c.ontop            end),
     awful.key({ modkey,           }, "t",      awful.client.floating.toggle                     ),
-    awful.key({ modkey, "Shift"   }, "t",
-      function (c)
-        local maximized = c.maximized_horizontal
-        c.maximized_horizontal = not maximized
-        c.maximized_vertical   = not maximized
-    end),
+    awful.key({ modkey, "Shift"   }, "t", function (c)
+                                                    local maximized = c.maximized_horizontal
+                                                    c.maximized_horizontal = not maximized
+                                                    c.maximized_vertical   = not maximized
+                                                end),
     awful.key({ modkey,           }, "a",      function (c) c.sticky = not c.sticky          end),
     awful.key({ modkey,           }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey, "Shift"   }, "q",      function (c)
