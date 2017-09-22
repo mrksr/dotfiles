ae9ba5a81b817df18e6cacbe59b793b052f7382f
Markus Kaiser <markus.kaiser@in.tum.de>
2017-09-22 18:51:59 +0100

    Fix wrong sorting of tags after screen move

--- awesome/.config/awesome/sharetags.lua
+++ awesome/.config/awesome/sharetags.lua
@@ -1,29 +1,49 @@
 local ipairs = ipairs
 local awful = require("awful")
-local capi = {
-  screen = screen
-}
 
 local sharetags = { }
 
 function sharetags.create_tags(names, layout)
-  local primary = capi.screen.primary
+  local primary = screen.primary
 
-  awful.tag(names, primary, layout)
+  for ix, name in ipairs(names) do
+    awful.tag.add(name, {
+      screen = primary,
+      layout = layout,
+      shared_index = ix,
+    })
+  end
+
   sharetags.tags = primary.tags
   return sharetags.tags
 end
 
 function sharetags.reset_tags(tags)
   for _, tag in ipairs(tags) do
-    sharetags.move(tag, capi.screen.primary)
+    sharetags.move(tag, screen.primary)
+  end
+end
+
+function sharetags.sort_tags(screen)
+  if not screen then
+    return
+  end
+
+  local tags_copy = {unpack(screen.tags)}
+  table.sort(tags_copy, function(a, b) return a.shared_index < b.shared_index end)
+
+  for ix, tag in ipars(tags_copy) do
+    tag.index = ix
   end
 end
 
 function sharetags.move(tag, to_screen)
-  if not tag or not to_screen then return end
+  if not tag or not to_screen then
+    return
+  end
 
-  if not tag.screen or to_screen ~= tag.screen then
+  local from_screen = tag.screen
+  if not from_screen or to_screen ~= from_scren then
     tag.screen = to_screen
     -- switch for all clients on tag
     for _ , client in ipairs(tag:clients()) do
@@ -34,6 +54,9 @@ function sharetags.move(tag, to_screen)
         awful.client.toggletag(tag, client)
       end
     end
+
+    sharetags.sort_tags(from_screen)
+    sharetags.sort_tags(to_screen)
   end
 end
 
