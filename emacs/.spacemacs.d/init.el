af7c95c54dd2756d3d9d08b0bb2a0eb35feb79ca
Markus Kaiser <markus.kaiser@siemens.com>
2018-09-14 10:01:17 +0200

    Add alternative completion hotkey

--- emacs/.spacemacs.d/init.el
+++ emacs/.spacemacs.d/init.el
@@ -10,6 +10,7 @@
       auto-completion-enable-sort-by-usage t
       auto-completion-return-key-behavior 'complete
       auto-completion-tab-key-behavior 'cycle
+      auto-completion-complete-with-key-sequence "jk"
       auto-completion-enable-help-tooltip t
       :disabled-for org erc
       )
