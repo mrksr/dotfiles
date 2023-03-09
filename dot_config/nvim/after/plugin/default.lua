aca492ef8fd1797173173dd09606255d12324a91
Markus Kaiser <mk2092@cam.ac.uk>
2023-03-09 19:02:24 +0000

    Fix typo

--- dot_config/nvim/after/plugin/default.lua
+++ dot_config/nvim/after/plugin/default.lua
@@ -17,5 +17,5 @@ if not vim.g.vscode then
 
     vim.keymap.set('n', '<leader>fs', ":w<CR>", { desc = '[F]ile [S]ave' })
     vim.keymap.set('n', '<leader>ss', require('telescope.builtin').current_buffer_fuzzy_find,
-        { desc = '[S]earch in current buffer ([S]ceen maybe?)' })
+        { desc = '[S]earch in current buffer' })
 end
