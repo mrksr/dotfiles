0a9dbed3cda47461f8975d586843936c36bf4265
Markus Kaiser <mk2092@cam.ac.uk>
2023-06-16 21:56:26 +0100

    Fix vscode plugin init

--- dot_config/nvim/lua/custom/init_vscode.lua
+++ dot_config/nvim/lua/custom/init_vscode.lua
@@ -1,14 +1,31 @@
 ---@diagnostic disable: undefined-global
-require('packer').startup(function(use)
-    use({
+--
+-- Install package manager
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    {
         "ggandor/leap.nvim",
         config = function()
             require("leap").add_default_mappings()
         end
-    })
-    use("machakann/vim-sandwich")
-    use("svermeulen/vim-cutlass")
-end)
+    },
+    "machakann/vim-sandwich",
+    "svermeulen/vim-cutlass"
+}, {})
 
 -- From kickstart.nvim
 vim.o.undofile = true
