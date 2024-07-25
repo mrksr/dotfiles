5ea37d05dfd8b08f419f0fbce0598fdc8f21e51b
Markus Kaiser <git@zfix.org>
2024-07-25 22:54:10 +0200

    Install in parallel

--- run_onchange_after_install_asdf_dependencies.sh
+++ run_onchange_after_install_asdf_dependencies.sh
@@ -4,24 +4,28 @@ source ~/.asdf/asdf.sh
 
 asdf plugin update --all
 
-asdf plugin add bat
-asdf plugin add bottom
-asdf plugin add delta
-asdf plugin add dust
-asdf plugin add eza
-asdf plugin add fzf
-asdf plugin add python
-asdf plugin add ripgrep
-asdf plugin add starship
+asdf plugin add bat &
+asdf plugin add bottom &
+asdf plugin add delta &
+asdf plugin add dust &
+asdf plugin add eza &
+asdf plugin add fzf &
+asdf plugin add python &
+asdf plugin add ripgrep &
+asdf plugin add starship &
 
-asdf install bat latest
-asdf install bottom latest
-asdf install delta latest
-asdf install dust latest
-asdf install eza latest
-asdf install fzf latest
-asdf install ripgrep latest
-asdf install starship latest
+wait
+
+asdf install bat latest &
+asdf install bottom latest &
+asdf install delta latest &
+asdf install dust latest &
+asdf install eza latest &
+asdf install fzf latest &
+asdf install ripgrep latest &
+asdf install starship latest &
+
+wait
 
 asdf global bat latest
 asdf global bottom latest
