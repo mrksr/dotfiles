879cc09972a3e647271d5b2e8591718c18b1ebde
Markus Kaiser <git@zfix.org>
2025-04-13 15:14:35 +0200

    Run mise in zsh

--- run_onchange_after_install_mise_en_place.zsh
+++ run_onchange_after_install_mise_en_place.zsh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env zsh
 
 curl https://mise.run | MISE_QUIET=1 sh
-eval "$(~/.local/bin/mise activate)"
+eval "$(~/.local/bin/mise activate zsh)"
 mise self-update --yes
 mise install --yes
 mise upgrade --yes
