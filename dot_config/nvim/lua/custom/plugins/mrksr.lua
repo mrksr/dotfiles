a2eefe22421eefb0c976bc172567f549abc5d458
Markus Kaiser <git@zfix.org>
2025-08-28 22:22:05 +0200

    Add Caddyfile plugin

--- dot_config/nvim/lua/custom/plugins/mrksr.lua
+++ dot_config/nvim/lua/custom/plugins/mrksr.lua
@@ -1,26 +1,27 @@
 ---@diagnostic disable: undefined-global
 return {
-    {
-        "RRethy/nvim-base16",
-        lazy = false,
-        config = function ()
-            if not vim.g.vscode then
-                vim.cmd.colorscheme "base16-railscasts"
-            end
-        end
-    },
-    {
-        "folke/which-key.nvim",
-        config = function()
-            require("which-key").setup({})
-        end
-    },
-    {
-        "ggandor/leap.nvim",
-        config = function()
-            require("leap").add_default_mappings()
-        end
-    },
-    "machakann/vim-sandwich",
-    "svermeulen/vim-cutlass"
+	{
+		"RRethy/nvim-base16",
+		lazy = false,
+		config = function()
+			if not vim.g.vscode then
+				vim.cmd.colorscheme("base16-railscasts")
+			end
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		config = function()
+			require("which-key").setup({})
+		end,
+	},
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").add_default_mappings()
+		end,
+	},
+	"machakann/vim-sandwich",
+	"svermeulen/vim-cutlass",
+	"isobit/vim-caddyfile",
 }
