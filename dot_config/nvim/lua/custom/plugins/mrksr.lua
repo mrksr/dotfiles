f5a9192a54bf6fbcc070cde9c4d727c685917f97
Markus Kaiser <git@zfix.org>
2025-11-11 00:16:52 +0100

    Maybe fix nvim-leap

--- dot_config/nvim/lua/custom/plugins/mrksr.lua
+++ dot_config/nvim/lua/custom/plugins/mrksr.lua
@@ -18,7 +18,8 @@ return {
 	{
 		"ggandor/leap.nvim",
 		config = function()
-			require("leap").add_default_mappings()
+			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+			vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
 		end,
 	},
 	"machakann/vim-sandwich",
