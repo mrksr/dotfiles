e5fb44244c639c6e5f2fd0356a4c4c36aed16228
Markus Kaiser <git@zfix.org>
2026-01-14 13:18:46 +0100

    Compactify vscode config

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -8,6 +8,7 @@ vim.keymap.set("v", "<leader>fw", "<cmd>'<,'>s/\\s\\+$//<CR>:let @/=''<cr>", { d
 
 if not vim.g.vscode then
   vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "File Save" })
+
   -- vim.keymap.set(
   --   "n",
   --   "<leader>ss",
@@ -22,43 +23,13 @@ if vim.g.vscode then
     vim.keymap.set("n", keys, func, { silent = true, remap = false })
   end
 
-  -- -- Editing
-  -- nmap(
-  --   "j",
-  --   "<Cmd>call VSCodeCall('cursorMove', { 'to': 'down', 'by': 'wrappedLine', 'value': v:count ? v:count : 1 })<CR>"
-  -- )
-  -- nmap(
-  --   "k",
-  --   "<Cmd>call VSCodeCall('cursorMove', { 'to': 'up', 'by': 'wrappedLine', 'value': v:count ? v:count : 1 })<CR>"
-  -- )
-
-  -- File Manipulation
   nmap("<leader>fs", "<Cmd>call VSCodeNotify('workbench.action.files.save')<CR>")
 
-  -- -- Project navigation
-  -- nmap("<leader>ff", "<Cmd>Find<CR>")
-  -- nmap("<leader>pf", "<Cmd>Find<CR>")
-  --
-  -- nmap("<leader>ps", "<Cmd>call VSCodeNotify('workbench.action.findInFiles')<CR>")
-  -- nmap("<leader>pj", "<Cmd>call VSCodeNotify('workbench.action.showAllSymbols')<CR>")
-  --
-  -- nmap("<leader>bb", "<Cmd>Tabfind<CR>")
-  --
-  -- nmap("<leader>ss", "<Cmd>call VSCodeNotify('fuzzySearch.activeTextEditor')<CR>")
-  -- nmap("<leader>sj", "<Cmd>call VSCodeNotify('workbench.action.gotoSymbol')<CR>")
-  --
-  -- -- Programming
-  -- nmap(
-  --   "<leader>fi",
-  --   "<Cmd>call VSCodeNotify('editor.action.formatDocument')<CR>:<C-u>call VSCodeNotify('editor.action.organizeImports')<CR>"
-  -- )
-  -- nmap("<leader>fx", "<Cmd>call VSCodeNotify('editor.action.quickFix')<CR>")
-  -- nmap("<leader>fr", "<Cmd>call VSCodeNotify('editor.action.rename')<CR>")
-  -- nmap("K", "<Cmd>call VSCodeNotify('editor.action.showHover')<CR>")
+  nmap(
+    "<leader>fi",
+    "<Cmd>call VSCodeNotify('editor.action.formatDocument')<CR>:<C-u>call VSCodeNotify('editor.action.organizeImports')<CR>"
+  )
+  nmap("K", "<Cmd>call VSCodeNotify('editor.action.showHover')<CR>")
   -- nmap("gd", "<Cmd>call VSCodeNotify('editor.action.peekDefinition')<CR>")
   -- nmap("gD", "<Cmd>call VSCodeNotify('editor.action.revealDefinition')<CR>")
-  --
-  -- -- Commenting
-  -- vim.keymap.set({ "n", "x", "o" }, "gc", "<plug>VSCodeCommentary", { silent = true })
-  -- vim.keymap.set("n", "gcc", "<plug>VSCodeCommentaryLine", { silent = true })
 end
