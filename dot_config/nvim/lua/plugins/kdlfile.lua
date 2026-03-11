df9e46d3c83519682c6089f2df6755c7962b5a0a
Markus Kaiser <git@zfix.org>
2026-03-11 20:16:33 +0100

    Fix kdlfile formatting; re-split niri config

--- dot_config/nvim/lua/plugins/kdlfile.lua
+++ dot_config/nvim/lua/plugins/kdlfile.lua
@@ -8,7 +8,7 @@ return {
       formatters = {
         kdl = {
           command = "kdlfmt",
-          args = { "format", "-" },
+          ergs = { "format", "--kdl-version=v1", "-" },
           stdin = true,
         },
       },
